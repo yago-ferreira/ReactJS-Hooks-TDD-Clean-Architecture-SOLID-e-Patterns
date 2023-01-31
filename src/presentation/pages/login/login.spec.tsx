829eb68cfc6659f86f40fae3493397acfd0c66fe
Yago Ferreira <yago.rox@hotmail.com>
2023-01-31 08:30:51 -0300

    Validation

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,17 +1,63 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult, fireEvent,cleanup } from '@testing-library/react'
 import Login from './login'
+import { Validation } from '@/presentation/protocols/validation'
+
+type SutTypes = { 
+    sut: RenderResult
+    validationSpy: ValidationSpy
+} 
+
+class ValidationSpy implements Validation { 
+    errorMessage: string
+    input: object
+
+    validate (input: object): string {
+        this.input = input
+        return this.errorMessage
+
+    }
+}
+
+
+const makeSut = (): SutTypes => {
+    const validationSpy = new ValidationSpy()
+    const sut  = render(<Login validation={validationSpy} />)
+    return {
+        sut,  
+        validationSpy
+    }
+}
 
 describe('Login Component', () => {
+    afterEach(cleanup)
+
+
     test('Should start with initial state', () =>{
-        const { getByTestId } = render(<Login />)
-        const errorWrap = getByTestId('error-wrap')
+        const { sut, validationSpy } = makeSut()
+        const errorWrap = sut.getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
-        const submitButton = getByTestId('submit') as HTMLButtonElement
+        const submitButton = sut.getByTestId('submit') as HTMLButtonElement
         expect(submitButton.disabled).toBe(true)
         // const emailStatus = getByTestId('email-status')
         // expect(emailStatus.title).toBe('Campo obrigatório')
         // const passwordStatus = getByTestId('password-status')
         // expect(passwordStatus.title).toBe('Campo obrigatório')
+    })
+    test('Should call Validation with corret values', () =>{
+        const { sut } = makeSut()
+        const emailInput = sut.getByTestId('email')
+        fireEvent.input(emailInput, { targert: { value: 'any_email' } })
+        expect(validationSpy.input).toEqual({ 
+            email: 'any_email'
+        })
+    })
+    test('Should call Validation with corret password', () =>{
+        const { sut } = makeSut()
+        const passwordInput = sut.getByTestId('password')
+        fireEvent.input(passwordInput, { targert: { value: 'any_password' } })
+        expect(validationSpy.input).toEqual({
+            password: 'any_password'
+        })
     })
 })
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,31 +1,39 @@
-import React, { useState }from 'react'
+import React, { useState, useEffect }from 'react'
 import Styles from './login-styles.scss'
-import { FormStatus, Header, Footer } from '@/presentation/components'
+import { FormStatus, Header, Footer, Input } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
+import { Validation } from '@/presentation/protocols/validation'
+
+type Props = {
+    validation: Validation
+}
 
 
 
-const Login: React.FC = () => {
-    const [state] = useState({
-        isLoading: false
+const Login: React.FC<Props> = ({ validation }: Props) => {
+    const [state, setState] = useState({
+        isLoading: false,
+        email: '',
+        password: '',
+        emailError: 'Campo obrigatório',
+        passwordEror: 'Campo obrigatório',
+        mainError: ''
     })
-    const [errorState] = useState({
-        email: 'Campo obrigatório',
-        password: 'Campo obrigatório',
-        main: ''
-    })
+    useEffect( () => {
+        validation.validate({ email: state.email })
+    }, [state.email])
+    useEffect( () => {
+        validation.validate({ password: state.password })
+    }, [state.password])
     return (
-        <div className={Styles.login}>
+        <div className={Styles.login}> 
             <Header />
-            <Context.Provider value={ {state, errorState} }>
+            <Context.Provider value={ { state, setState} }>
 
             <form className={Styles.form}>
                 <h2>Login</h2>
-                <div className={Styles.inputWrap}>
-                    <input type="email" name="email" placeholder="Digite seu e-mail" />
-                    <input type="password" name="password" placeholder="Digite sua senha" />
-                </div>
-               
+                <Input type="email" name="email" placeholder="Digite seu e-mail" />
+                <Input type="password" name="password" placeholder="Digite sua senha"  />
                 <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
                 <span className={Styles.link}>Criar Conta</span>
                 <FormStatus />
